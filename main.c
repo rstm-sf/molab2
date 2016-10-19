cf11e1e12e4985af7bdc670a89baabb9e1aecf90
Rustam Sayfutdinov <rstm.sf@yandex.ru>
2016-10-20 00:06:32 +0500

    Начало

---
+++ main.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <math.h>
+
+typedef struct point2d {
+	double x;
+	double y;
+} point2d_t;
+
+double fun(point2d_t x);
+double grad_fun(point2d_t x);
+
+uint32_t main() {
+
+	system("pause");
+
+	return 0;
+}
+
+inline double fun(const point2d_t x) {
+	return (x.x - 2.0 * x.y) * (x.x - 2.0 * x.y) + (x.x + 5.0) * (x.x + 5.0);
+}
+
+inline double grad_fun(const point2d_t x) {
+	return 20 + 8 * x.y;
+}
