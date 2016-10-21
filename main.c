6c026007defefdd43b998860281643c3f2092a34
Rustam Sayfutdinov <rstm.sf@yandex.ru>
2016-10-21 23:19:45 +0500

    Добавлен метод Ньютона-Рафсона

--- main.c
+++ main.c
@@ -10,17 +10,27 @@ typedef struct point2d {
 	double y;
 } point2d_t;
 
+typedef struct mat2x2 {
+	double a11, a12, a21, a22;
+} mat2x2_t;
+
 double dotProduct2d(point2d_t x, point2d_t y);
+point2d_t mat_vec2d(mat2x2_t mat, point2d_t x);
+double det_mat2x2(mat2x2_t mat);
+mat2x2_t inversed_mat2x2(mat2x2_t mat);
+bool checkPositiveDefMat2x2(mat2x2_t mat);
 double fun(point2d_t x);
 point2d_t grad_fun(point2d_t x);
+mat2x2_t fun_Hessian_mat2x2();
 double findT_phi(point2d_t x, point2d_t grad_fun_x);
 point2d_t nonlinearConjugateGradientMethod(point2d_t x0, double epsilon, uint32_t maxIter);
+point2d_t methodNewtonRaphson(point2d_t x0, double epsilon, uint32_t maxIter);
 
 int32_t main() {
-	const point2d_t x0     = { 0.0, 0.0 };
+	const point2d_t x0     = { -4.0, -2.5 };
 	const double epsilon   = 1.0e-2;
 	const uint32_t maxIter = 100;
-	const point2d_t xmin   = nonlinearConjugateGradientMethod(x0, epsilon, maxIter);
+	const point2d_t xmin   = methodNewtonRaphson(x0, epsilon, maxIter);
 
 	printf("\nXmin = (%.2f, %.2f)\n", xmin.x, xmin.y);
 
@@ -33,14 +43,51 @@ inline double dotProduct2d(const point2d_t x, const point2d_t y) {
 	return x.x * y.x + x.y * y.y;
 }
 
+inline point2d_t mat_vec2d(const mat2x2_t mat, const point2d_t x) {
+	const point2d_t vec = { mat.a11 * x.x + mat.a12 * x.y, mat.a21 * x.x + mat.a22 * x.y };
+	return vec;
+}
+
+inline double det_mat2x2(const mat2x2_t mat) {
+	return mat.a11 * mat.a22 - mat.a21 * mat.a12;
+}
+
+inline mat2x2_t inversed_mat2x2(const mat2x2_t mat) {
+	const double det_mat = det_mat2x2(mat);
+	if (det_mat == 0) {
+		printf("Determinant = 0!\n");
+		const mat2x2_t inv_mat2x2 = { 0, 0, 0, 0 };
+		return inv_mat2x2;
+	}
+	const double inv_det_mat  = 1.0 / det_mat;
+	const mat2x2_t inv_mat2x2 = { mat.a22 * inv_det_mat, -mat.a12 * inv_det_mat,
+		                         -mat.a21 * inv_det_mat,  mat.a11 * inv_det_mat };
+	return inv_mat2x2;
+}
+
+inline bool checkPositiveDefMat2x2(const mat2x2_t mat) {
+	// Sylvester's criterion
+	if (mat.a11 <= 0.0 && det_mat2x2(mat) <= 0) {
+		printf("No Positive-definite matrix!\n");
+		return false;
+	} else {
+		return true;
+	}
+}
+
 inline double fun(const point2d_t x) {
 	return (x.x - 2.0 * x.y) * (x.x - 2.0 * x.y) + (x.x + 5.0) * (x.x + 5.0);
 }
 
 inline point2d_t grad_fun(const point2d_t x) {
-	point2d_t grad = { 4.0 * x.x + 4.0 * x.y + 10.0,
-					   4.0 * x.x - 8.0 * x.y         };
+	const point2d_t grad = { 4.0 * x.x - 4.0 * x.y + 10.0,
+		                     8.0 * x.y - 4.0 * x.x         };
 	return grad;
+}
+
+inline mat2x2_t fun_Hessian_mat2x2() {
+	const mat2x2_t H = { 4, -4, -4, 8 };
+	return H;
 }
 
 inline double findT_phi(const point2d_t x, const point2d_t d) {
@@ -49,28 +96,29 @@ inline double findT_phi(const point2d_t x, const point2d_t d) {
 	//	printf("Error! d^phi/dt^2 < 0\n");
 	//	return 0;
 	//}
-	return ((2.0 * x.y - x.x) * (d.x - 2.0 * d.y) - (5.0 + x.x) * d.x) / dotProduct2d(d, d);
+	const double A = (d.x - 2.0 * d.y);
+	return (A * (2.0 * x.y - x.x) - (5.0 + x.x) * d.x) / (A * A + d.y * d.y);
 }
 
 point2d_t nonlinearConjugateGradientMethod(const point2d_t x0, const double epsilon, const uint32_t maxIter) {
 	const double epsilon2 = epsilon * epsilon;
 	point2d_t xk1 = x0;
-	point2d_t xk, d;
+	point2d_t xk, d, grad_fun_xk;
+	double dot_grad_xk;
 	bool is_seq = false;
 	uint32_t k = 0;
 
 	for (k; k < maxIter; ++k) {
 		const point2d_t grad_fun_xk1 = grad_fun(xk1);
-		const double norm_grad = dotProduct2d(grad_fun_xk1, grad_fun_xk1);
-		if (norm_grad < epsilon2)
+		const double    dot_grad_xk1 = dotProduct2d(grad_fun_xk1, grad_fun_xk1);
+		if (dot_grad_xk1 < epsilon2)
 			break;
 
-		if(k == 0) {
+		if (k == 0) {
 			d.x = -grad_fun_xk1.x;
 			d.y = -grad_fun_xk1.y;
 		} else {
-			const point2d_t grad_fun_xk = grad_fun(xk);
-			const double beta = dotProduct2d(grad_fun_xk1, grad_fun_xk1) / dotProduct2d(grad_fun_xk, grad_fun_xk);
+			const double beta = dot_grad_xk1 / dot_grad_xk;
 			d.x = beta * d.x - grad_fun_xk1.x;
 			d.y = beta * d.y - grad_fun_xk1.y;
 		}
@@ -78,12 +126,12 @@ point2d_t nonlinearConjugateGradientMethod(const point2d_t x0, const double epsi
 		xk = xk1;
 		const double t = findT_phi(xk, d);
 		const point2d_t xk1_minus_xk = { t * d.x, t * d.y };
-		xk1.x = xk.x + xk1_minus_xk.x;
-		xk1.y = xk.y + xk1_minus_xk.y;
+		xk1.x += xk1_minus_xk.x;
+		xk1.y += xk1_minus_xk.y;
 
-		const double norm_xk1_minus_xk = dotProduct2d(xk1_minus_xk, xk1_minus_xk);
-		const double abs_fk1_minus_fk  = fabs(fun(xk1) - fun(xk));
-		if (norm_xk1_minus_xk < epsilon2 && abs_fk1_minus_fk < epsilon) {
+		const double dot_xk1_minus_xk = dotProduct2d(xk1_minus_xk, xk1_minus_xk);
+		const double abs_fk1_minus_fk = fabs(fun(xk1) - fun(xk));
+		if (dot_xk1_minus_xk < epsilon2 && abs_fk1_minus_fk < epsilon) {
 			if (is_seq) {
 				break;
 			} else {
@@ -92,9 +140,61 @@ point2d_t nonlinearConjugateGradientMethod(const point2d_t x0, const double epsi
 		} else {
 			is_seq = false;
 		}
+		grad_fun_xk = grad_fun_xk1;
+		dot_grad_xk = dot_grad_xk1;
 	}
 
 	printf("Nonlinear Conjugate Gradient Method\nIters = %" PRIu32 "\n", k);
+
+	return xk1;
+}
+
+point2d_t methodNewtonRaphson(const point2d_t x0, const double epsilon, const uint32_t maxIter) {
+	const double epsilon2 = epsilon * epsilon;
+	point2d_t xk1 = x0;
+	point2d_t d;
+	bool is_seq = false;
+	uint32_t k = 0;
+
+	for (k; k < maxIter; ++k) {
+		const point2d_t grad_fun_xk1 = grad_fun(xk1);
+		const double    dot_grad_xk1 = dotProduct2d(grad_fun_xk1, grad_fun_xk1);
+		if (dot_grad_xk1 < epsilon2)
+			break;
+
+		const mat2x2_t H    = fun_Hessian_mat2x2();
+		const mat2x2_t invH = inversed_mat2x2(H);
+		if (checkPositiveDefMat2x2(invH)) {
+			const point2d_t tmp = mat_vec2d(H, grad_fun_xk1);
+			d.x = -tmp.x;
+			d.y = -tmp.y;
+		} else {
+			d.x = -grad_fun_xk1.x;
+			d.y = -grad_fun_xk1.y;
+		}
+
+		const point2d_t xk = xk1;
+		const double t = findT_phi(xk, d);
+		const point2d_t xk1_minus_xk = { t * d.x, t * d.y };
+		xk1.x += xk1_minus_xk.x;
+		xk1.y += xk1_minus_xk.y;
+
+		const double dot_xk1_minus_xk = dotProduct2d(xk1_minus_xk, xk1_minus_xk);
+		const double abs_fk1_minus_fk = fabs(fun(xk1) - fun(xk));
+		if (dot_xk1_minus_xk < epsilon2 && abs_fk1_minus_fk < epsilon) {
+			if (is_seq) {
+				break;
+			}
+			else {
+				is_seq = true;
+			}
+		}
+		else {
+			is_seq = false;
+		}
+	}
+
+	printf("Newton-Raphson Method\nIters = %" PRIu32 "\n", k);
 
 	return xk1;
 }
